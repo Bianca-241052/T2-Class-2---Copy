a1ce9c0d95e0c8913c06cbae5cbd819392641108
Bianca-241052 <159829372+Bianca-241052@users.noreply.github.com>
2024-04-29 18:48:26 +0200

    Basic js
    class 2 week 2

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+console.log("I am "+ 20 + " years old");
+console.log(15 + 16);
+
+let x = 5
+let y = 6
+let z = x + y;
+
+document.getElementById('math').innerHTML = "The value of Z is; " + z;
+
+function myFunction(){
+    alert ("How are you?")
+}
